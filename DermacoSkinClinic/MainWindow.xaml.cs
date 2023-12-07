ba47344ecb70efa63b936ae85f1abb6aeffe2fa7
Madhumitha_8911168 <mvijayakumar1168@conestogac.on.ca>
2023-12-06 20:19:28 -0500

    Added loginpage

--- DermacoSkinClinic/MainWindow.xaml.cs
+++ DermacoSkinClinic/MainWindow.xaml.cs
@@ -1,146 +1,38 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Linq;
+﻿// MainWindow.xaml.cs
+using DermacoSkinClinic;
 using System.Windows;
-using System.Windows.Controls;
 
-namespace DermacoSkinClinic
+namespace YourNamespace
 {
-    public partial class AppointmentForm : Window
+    public partial class MainWindow : Window
     {
-        public ObservableCollection<Appointment> Appointments { get; set; }
-
-        public AppointmentForm()
+        public MainWindow()
         {
             InitializeComponent();
-            Appointments = new ObservableCollection<Appointment>();
-            AppointmentDataGrid.ItemsSource = Appointments;
         }
 
-        private void SubmitAppointment_Click(object sender, RoutedEventArgs e)
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            string firstName = FirstNameTextBox.Text;
-            string lastName = LastNameTextBox.Text;
-            string email = EmailTextBox.Text;
-            string Comments = CommentsTextBox.Text;
-            string selectedTime = AppointmentTimeComboBox.Text;
-            //ComboBoxItem selectedItem = AppointmentTimeComboBox.SelectedItem as ComboBoxItem;
+            // Implement your authentication logic here
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
 
-            //  DateTime appointDate = AppointmentDateTimePicker.SelectedDate;
-            // Continue retrieving data for other controls (LastNameTextBox, EmailTextBox, AppointmentDatePicker, AppointmentTimePicker)
-
-            // Create a new Appointment object with the captured data
-            Appointment newAppointment = new Appointment
+            // Check username and password (example: hardcoded for demonstration)
+            if (username == "user" && password == "password")
             {
-                FirstName = firstName,
-                LastName = lastName,
-                Email = email,
-                Comments = Comments,
-                AppointmentDate = (DateTime)AppointmentDateTimePicker.SelectedDate,
-                AppointmentTime = selectedTime,
-                //AppointmentTime = (TimeSpan)AppointmentTimeComboBox.SelectedItem,
-                // Set other properties...
-            };
+                // Navigate to the next page (MainWindow2 for example)
+                AppointmentForm appointmentForm = new AppointmentForm();
+                appointmentForm.Show();
 
-
-            // Add the new appointment to the ObservableCollection
-            Appointments.Add(newAppointment);
-
-            // Clear form fields
-            ClearFormFields();
-        }
-        private void ClearButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Clear the text in the textboxes
-            FirstNameTextBox.Text = string.Empty;
-            LastNameTextBox.Text = string.Empty;
-            EmailTextBox.Text = string.Empty;
-            PhoneTextBox.Text = string.Empty;
-            PostalCodeTextBox.Text = string.Empty;
-            CommentsTextBox.Text = string.Empty;
-            ProcedureComboBox.SelectedIndex = -1;
-            EmailContactCheckBox.IsChecked = false;
-            PhoneContactCheckBox.IsChecked = false;
-            DOBDatePicker.SelectedDate = null;
-            AppointmentTimeComboBox.SelectedIndex = -1;
-            EmailSubscriptionCheckBox.IsChecked = false;
-            TermsCheckBox.IsChecked = false;
-            AppointmentDateTimePicker.SelectedDate = null;
-            // EmailSubscriptionCheckBox.IsEnabled; 
-            //TermsCheckBox.
-            //  AppointmentDataGrid
-
-
-
-        }
-
-        private void ApplyFilter_Click(object sender, RoutedEventArgs e)
-        {
-            // Add logic to apply filter to the displayed data
-            // Example: Filter appointments based on user input
-            string searchText = SearchTextBox.Text;
-            string filterOption = FilterComboBox.SelectedItem?.ToString();
-
-            if (filterOption == "Filter by Name")
-            {
-                var filteredAppointments = Appointments.Where(appointment =>
-                    appointment.FirstName.Contains(searchText) || appointment.LastName.Contains(searchText)).ToList();
-
-                AppointmentDataGrid.ItemsSource = filteredAppointments;
-            }
-            else if (filterOption == "Filter by Email")
-            {
-                var filteredAppointments = Appointments.Where(appointment =>
-                    appointment.Email.Contains(searchText)).ToList();
-
-                AppointmentDataGrid.ItemsSource = filteredAppointments;
+                // Close the current login window
+                this.Close();
             }
             else
             {
-                // Reset the filter if no valid filter option is selected
-                AppointmentDataGrid.ItemsSource = Appointments;
+                MessageBox.Show("Invalid username or password. Please try again.");
             }
         }
-
-        private void ClearFormFields()
-        {
-            FirstNameTextBox.Clear();
-            LastNameTextBox.Clear();
-            EmailTextBox.Clear();
-            PhoneTextBox.Clear();
-            PostalCodeTextBox.Clear();
-            DOBDatePicker.SelectedDate = null;
-            EmailContactCheckBox.IsChecked = false;
-            ProcedureComboBox.SelectedIndex = -1;
-            // Clear other form fields...
-            AppointmentDateTimePicker.SelectedDate = null;
-            AppointmentTimeComboBox.SelectedIndex = -1;
-            CommentsTextBox.Clear();
-            EmailSubscriptionCheckBox.IsChecked = false;
-            TermsCheckBox.IsChecked = false;
-        }
-
-        private void AppointmentTimeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void ClearFilter_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-    }
-
-    public class Appointment
-    {
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string Comments { get; set; } = string.Empty;
-        public DateTime AppointmentDate { get; set; }
-        public string AppointmentTime { get; set; }
-        public bool IsEmailSubscriptionEnabled { get; set; }
-        public bool IsTermsAccepted { get; set; }
-        public DateTime AppointmentDateTime { get; set; }
     }
 }
+
+
