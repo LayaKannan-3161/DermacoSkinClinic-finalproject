6e608cca72ce35ebae5e95316c34807c6afeaa4e
Laya Kannan <Lkannan3161@conestogac.on.ca>
2023-12-08 15:54:06 -0500

    Laya: Fixed  the issues in previous code push and now the program is running properly.

--- DermacoSkinClinic/AppointmentForm.xaml.cs
+++ DermacoSkinClinic/AppointmentForm.xaml.cs
@@ -23,115 +23,61 @@ namespace DermacoSkinClinic
             string lastName = LastNameTextBox.Text;
             string email = EmailTextBox.Text;
             string phone = PhoneTextBox.Text;
-            string Comments = CommentsTextBox.Text;
-            string selectedTime = AppointmentTimeComboBox.Text
-            // Validate first name
+            string comments = CommentsTextBox.Text;
+            string selectedTime = AppointmentTimeComboBox.Text;
+
             if (string.IsNullOrWhiteSpace(firstName) || !IsValidName(firstName))
             {
                 MessageBox.Show("Please enter a valid first name.", "Invalid First Name", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
 
-            // Validate last name
             if (string.IsNullOrWhiteSpace(lastName) || !IsValidName(lastName))
             {
                 MessageBox.Show("Please enter a valid last name.", "Invalid Last Name", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
-            // Validate email
+
             if (!IsValidEmail(email))
             {
                 MessageBox.Show("Please enter a valid email address.", "Invalid Email", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
+
             if (!IsValidPhoneNumber(phone))
             {
                 MessageBox.Show("Please enter a valid Canadian phone number.", "Invalid Phone Number", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
+
             if (!TermsCheckBox.IsChecked.GetValueOrDefault())
             {
                 MessageBox.Show("Please accept the terms and conditions.", "Terms and Conditions", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
-                //ComboBoxItem selectedItem = AppointmentTimeComboBox.SelectedItem as ComboBoxItem;
+            }
 
-                //  DateTime appointDate = AppointmentDateTimePicker.SelectedDate;
-                // Continue retrieving data for other controls (LastNameTextBox, EmailTextBox, AppointmentDatePicker, AppointmentTimePicker)
-
-                // Create a new Appointment object with the captured data
-                Appointment newAppointment = new Appointment
+            Appointment newAppointment = new Appointment
             {
                 FirstName = firstName,
                 LastName = lastName,
                 Email = email,
-                Comments = Comments,
+                Comments = comments,
                 AppointmentDate = (DateTime)AppointmentDateTimePicker.SelectedDate,
                 AppointmentTime = selectedTime,
-                //AppointmentTime = (TimeSpan)AppointmentTimeComboBox.SelectedItem,
-                // Set other properties...
             };
 
-
-            // Add the new appointment to the ObservableCollection
             Appointments.Add(newAppointment);
 
-            // Clear form fields
             ClearFormFields();
         }
 
-            private bool IsValidEmail(string email)
-            {
-                try
-                {
-                    var mailAddress = new System.Net.Mail.MailAddress(email);
-                    return mailAddress.Address == email;
-                }
-                catch
-                {
-                    return false;
-                }
-            }
-            private bool IsValidName(string name)
-            {
-                return name.All(char.IsLetter);
-            }
-            private bool IsValidPhoneNumber(string phoneNumber)
-            {
-                // Remove non-digit characters from the phone number
-                string cleanedNumber = new string(phoneNumber.Where(char.IsDigit).ToArray());
-
-                // Validate the cleaned number against the Canadian phone number pattern eg (123) 456-7890
-                return System.Text.RegularExpressions.Regex.IsMatch(cleanedNumber, @"^(\d{10}|\(\d{3}\)\s?\d{3}[-\.\s]?\d{4}|\d{3}[-\.\s]?\d{4})$");
-            }
-            private void ClearButton_Click(object sender, RoutedEventArgs e)
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
-            // Clear the text in the textboxes
-            FirstNameTextBox.Text = string.Empty;
-            LastNameTextBox.Text = string.Empty;
-            EmailTextBox.Text = string.Empty;
-            PhoneTextBox.Text = string.Empty;
-            PostalCodeTextBox.Text = string.Empty;
-            CommentsTextBox.Text = string.Empty;
-            ProcedureComboBox.SelectedIndex = -1;
-            EmailContactCheckBox.IsChecked = false;
-            PhoneContactCheckBox.IsChecked = false;
-            DOBDatePicker.SelectedDate = null;
-            AppointmentTimeComboBox.SelectedIndex = -1;
-            EmailSubscriptionCheckBox.IsChecked = false;
-            TermsCheckBox.IsChecked = false;
-            AppointmentDateTimePicker.SelectedDate = null;
-            // EmailSubscriptionCheckBox.IsEnabled; 
-            //TermsCheckBox.
-            //  AppointmentDataGrid
-
-
-
+            ClearFormFields();
         }
 
         private void ApplyFilter_Click(object sender, RoutedEventArgs e)
         {
-            // Add logic to apply filter to the displayed data
-            // Example: Filter appointments based on user input
             string searchText = SearchTextBox.Text;
             string filterOption = FilterComboBox.SelectedItem?.ToString();
 
@@ -151,9 +97,31 @@ namespace DermacoSkinClinic
             }
             else
             {
-                // Reset the filter if no valid filter option is selected
                 AppointmentDataGrid.ItemsSource = Appointments;
             }
+        }
+
+        private void ClearFilter_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear filter logic, if needed
+            AppointmentDataGrid.ItemsSource = Appointments;
+            FilterComboBox.SelectedIndex = -1;
+            SearchTextBox.Text = string.Empty;
+        }
+
+        private void TermsCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // Handle terms checkbox checked event, if needed
+        }
+
+        private void TermsCheckBox_Checked_1(object sender, RoutedEventArgs e)
+        {
+            // Handle terms checkbox checked event, if needed
+        }
+
+        private void EmailSubscriptionCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // Handle email subscription checkbox checked event, if needed
         }
 
         private void ClearFormFields()
@@ -163,49 +131,49 @@ namespace DermacoSkinClinic
             EmailTextBox.Clear();
             PhoneTextBox.Clear();
             PostalCodeTextBox.Clear();
-            DOBDatePicker.SelectedDate = null;
-            EmailContactCheckBox.IsChecked = false;
-            ProcedureComboBox.SelectedIndex = -1;
-            // Clear other form fields...
-            AppointmentDateTimePicker.SelectedDate = null;
-            AppointmentTimeComboBox.SelectedIndex = -1;
             CommentsTextBox.Clear();
+            ProcedureComboBox.SelectedIndex = -1;
+            EmailContactCheckBox.IsChecked = false;
+            PhoneContactCheckBox.IsChecked = false;
+            DOBDatePicker.SelectedDate = null;
+            AppointmentTimeComboBox.SelectedIndex = -1;
             EmailSubscriptionCheckBox.IsChecked = false;
             TermsCheckBox.IsChecked = false;
+            AppointmentDateTimePicker.SelectedDate = null;
         }
 
-        private void AppointmentTimeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private bool IsValidEmail(string email)
         {
-
+            try
+            {
+                var mailAddress = new System.Net.Mail.MailAddress(email);
+                return mailAddress.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
-        private void ClearFilter_Click(object sender, RoutedEventArgs e)
+        private bool IsValidName(string name)
         {
-
+            return name.All(char.IsLetter);
         }
 
-        private void TermsCheckBox_Checked(object sender, RoutedEventArgs e)
+        private bool IsValidPhoneNumber(string phoneNumber)
         {
-
+            string cleanedNumber = new string(phoneNumber.Where(char.IsDigit).ToArray());
+            return System.Text.RegularExpressions.Regex.IsMatch(cleanedNumber, @"^(\d{10}|\(\d{3}\)\s?\d{3}[-\.\s]?\d{4}|\d{3}[-\.\s]?\d{4})$");
         }
 
-        private void TermsCheckBox_Checked_1(object sender, RoutedEventArgs e)
+        public class Appointment
         {
-
+            public string FirstName { get; set; } = string.Empty;
+            public string LastName { get; set; } = string.Empty;
+            public string Email { get; set; } = string.Empty;
+            public string Comments { get; set; } = string.Empty;
+            public DateTime AppointmentDate { get; set; }
+            public string AppointmentTime { get; set; }
         }
-    }
-
-    public class Appointment
-    {
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string Comments { get; set; } = string.Empty;
-        public DateTime AppointmentDate { get; set; }
-        public string AppointmentTime { get; set; }
-        public bool IsEmailSubscriptionEnabled { get; set; }
-        public bool IsTermsAccepted { get; set; }
-        public DateTime AppointmentDateTime { get; set; }
     }
 }
-
